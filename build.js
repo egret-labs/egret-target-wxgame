464b839a0aa669e6b265ba695070de8d25b67c27
himuil <jerry.lin@egret.com>
2018-06-04 14:14:49 +0800

    Modify version

--- build.js
+++ build.js
@@ -8,15 +8,21 @@ var version = commands[0];
 var cp = require('child_process');
 var fs = require('fs');
 
+var targetJsonPath = "./target/target.json";
+var targetJson = JSON.parse(fs.readFileSync(targetJsonPath, { encoding }));
+targetJson.version = version;
+fs.writeFileSync(targetJsonPath, JSON.stringify(targetJson, undefined, "\t"));
+
 var encoding = "utf8";
 var exe = cp.exec("egret build", function () {
-    var content = fs.readFileSync("./target/template/egret.wxgame.js", { encoding });
+    var jsPath = "./target/template/egret.wxgame.js";
+    var content = fs.readFileSync(jsPath, { encoding });
     content = content.replace(/var egret;/gi, "");
     var key = "wxgame.version = \"";
     var index = content.indexOf(key);
     content = content.slice(0, index + key.length) + version + content.slice(index + key.length + 5);
-    fs.writeFileSync("./target/template/egret.wxgame.js", content);
-    fs.unlinkSync("./target/template/egret.wxgame.min.js");
+    fs.writeFileSync(jsPath, content);
+    fs.unlinkSync(jsPath.replace(".js", ".min.js"));
 });
 
 exe.stdout.on("data", function (data) {
