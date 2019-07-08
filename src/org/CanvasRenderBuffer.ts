c2d675903baab50ebc96dd0dd3274e9248fed9ed
张宇 <runinspring@gmail.com>
2019-07-08 15:08:26 +0800

    update

--- src/org/CanvasRenderBuffer.ts
+++ src/org/CanvasRenderBuffer.ts
@@ -33,11 +33,7 @@ namespace egret.wxgame {
      * 创建一个canvas。
      */
     function __createCanvas__(width?: number, height?: number): HTMLCanvasElement {
-        let canvas: HTMLCanvasElement = document.createElement("canvas");
-        if (!isNaN(width) && !isNaN(height)) {
-            canvas.width = width;
-            canvas.height = height;
-        }
+        let canvas = egret.sys.createCanvas(width, height);
         let context = canvas.getContext("2d");
         if (context["imageSmoothingEnabled"] === undefined) {
             let keys = ["webkitImageSmoothingEnabled", "mozImageSmoothingEnabled", "msImageSmoothingEnabled"];
@@ -80,6 +76,7 @@ namespace egret.wxgame {
                 this.context.$offsetX = 0;
                 this.context.$offsetY = 0;
             }
+            this.resize(width, height)
         }
 
         /**
